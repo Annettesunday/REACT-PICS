fb2e56560ceff2cf510265a224a706624518bb8b
annettesunday <annette.odhiambo@andela.com>
2019-07-08 05:43:15 +0300

    ch(SearchBar): show forms to users and add a touch of style

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactDom from "react-dom";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container" style={{ marginTop: "10px" }}>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
