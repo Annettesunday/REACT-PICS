af223ffc89c48a2762b6cbf50c5ef034a822da2e
annettesunday <annette.odhiambo@andela.com>
2019-07-08 10:58:19 +0300

    ch(app): seeting state after async requests

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,24 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID 6629ef94a85a23b9fb7e046d26d0d148b594ab49e32078e0923a3126a630aff8"
-        }
-      });
-
-      console.log(response.data.results)
+  state = {
+    images: []
   };
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 6629ef94a85a23b9fb7e046d26d0d148b594ab49e32078e0923a3126a630aff8"
+      }
+    });
+    this.setState({ images: response.data.results });
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
